4716088756fa7cb344d55cfb0471e17a13cf43fa
kcloud721 <kcloud721@naver.com>
2020-10-05 18:05:51 +0900

    TIL 1005 R: basic & data.frame load

---
+++ bigdata-iot/R/1005-기본,데이터프레임/1005-p77.R
@@ -0,0 +1,9 @@
+#교재 p.77 연습문제
+
+ID=c("1", "2", "3", "4", "5")
+MID_EXAM=c(10, 25, 100, 75, 30)
+CLASS=c("1반", "2반", "3반", "1반", "2반")
+
+example_test <- data.frame(ID, MID_EXAM, CLASS)
+example_test
+
---
+++ bigdata-iot/R/1005-기본,데이터프레임/ws02.R
@@ -0,0 +1,4 @@
+library(readxl)
+ws02 <- read.csv("R-Data/2020bus2.csv",
+                 sep=",", head=TRUE, encoding="UTF-8")
+ws02
