b8de683a0799cda92bca60dbcc8bc4b80fc10941
kcloud721 <kcloud721@naver.com>
2020-10-23 14:23:52 +0900

    WS-func2: MapView

--- bigdata-iot/Android/Workshop2/app/src/main/java/com/example/workshop2/Fragment2.java
+++ bigdata-iot/Android/Workshop2/app/src/main/java/com/example/workshop2/Fragment2.java
@@ -1,21 +1,63 @@
 package com.example.workshop2;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class Fragment2 extends Fragment {
+    LocationManager locationManager;
+    SupportMapFragment supportMapFragment;
+    MapView gmap;
+
     public Fragment2() {
+
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_2, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_2, container, false);
+        gmap = (MapView) v.findViewById(R.id.map);
+        gmap.onCreate(savedInstanceState);
+        gmap.onResume();
+
+        MapsInitializer.initialize(getActivity().getApplicationContext());
+
+        gmap.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng latlng = new LatLng(34.1742, -118.4580);
+//                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    return;
+//                }
+                // googleMap.setMyLocationEnabled(true);
+                googleMap.addMarker(
+                        new MarkerOptions().position(latlng).
+                                title("공항").snippet("xxx")
+                );
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,10));
+            }
+        });
+
+        return v;
     }
 }
