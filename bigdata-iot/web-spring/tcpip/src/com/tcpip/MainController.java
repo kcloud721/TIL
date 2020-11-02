26a82c18b2bc583522db550073982ebedb1a2bc6
kcloud721 <kcloud721@naver.com>
2020-11-02 15:12:28 +0900

    TCP/IP & Spring : Spring(MVC)

--- bigdata-iot/web-spring/tcpip/src/com/tcpip/MainController.java
+++ bigdata-iot/web-spring/tcpip/src/com/tcpip/MainController.java
@@ -1,13 +1,47 @@
 package com.tcpip;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.chat.Client;
+
+@Controller
 public class MainController {
+	
+	Client client;
+	
+	public MainController() {
+		client = new Client("192.168.0.6",5555,"[KKKK]");
+		try {
+			client.connect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@RequestMapping("/main.mc")
 	public ModelAndView main() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("main");
 		return mv;
 	}
+	@RequestMapping("/iot.mc")
+	public void iot(HttpServletResponse res) throws IOException {		// 화면이동 없으므로  void
+		System.out.println("IoT Send Start...");
+		client.sendTarget("/192.168.0.6", "100");
+		PrintWriter out = res.getWriter();
+		out.print("ok");
+		out.close();
+	}
+	@RequestMapping("/phone.mc")
+	public void phone() {		// 화면이동 없으므로  void
+		System.out.println("Phone Send Start...");
+	}
+	
 }
