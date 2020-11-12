f4fa2835337780fed3f6772702561b12c241c31b
kcloud721 <kcloud721@naver.com>
2020-11-12 17:20:01 +0900

    아두이노 워크샵

--- bigdata-iot/Arduino/_20201112_ws/_20201112_ws.ino
+++ bigdata-iot/Arduino/_20201112_ws/_20201112_ws.ino
@@ -1,3 +1,5 @@
+const int tempPin = A0;
+
 void setup() {
   Serial.begin(9600);
 }
@@ -5,12 +7,17 @@ void setup() {
 void loop() {
   char cmd = "";
   Serial.println("READY");
+
   if(Serial.available() > 0){
     cmd = Serial.read();
     cmd = cmd.substring(7, cmd.length()); //:G11A9\n 제거 
     Serial.println(cmd);
+
     if(cmd == 's'){
       Serial.println("SENSEOR Start!");
+      int data = analogRead(tempPin);
+      float temp = ((5.0 * data) / 1024.0) * 100;
+      Serial.println(temp);
     } else if(cmd == 't'){
       Serial.println("SENSEOR STOP!");
     }
