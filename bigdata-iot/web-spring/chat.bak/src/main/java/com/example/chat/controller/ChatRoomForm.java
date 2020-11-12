7272bbdc8cfc4b76862aa9c811c723f27d40d277
kcloud721 <kcloud721@naver.com>
2020-11-12 17:42:12 +0900

    chat.bak 삭제

--- bigdata-iot/web-spring/chat.bak/src/main/java/com/example/chat/controller/ChatRoomForm.java
+++
@@ -1,9 +0,0 @@
-package com.example.chat.controller;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class ChatRoomForm {
-    private String name;
-}
--- bigdata-iot/web-spring/chat.bak/src/main/java/com/example/chat/domain/MessageType.java
+++
@@ -1,5 +0,0 @@
-package com.example.chat.domain;
-
-public enum MessageType {
-    ENTER,CHAT,LEAVE
-}
