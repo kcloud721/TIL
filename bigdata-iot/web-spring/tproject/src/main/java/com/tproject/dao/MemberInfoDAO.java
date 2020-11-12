c1543b6ddcb10ee29d387c5c02a50084b0fb445f
kcloud721 <kcloud721@naver.com>
2020-11-12 17:41:10 +0900

    tproject 삭제

--- bigdata-iot/web-spring/tproject/src/main/java/com/tproject/dao/MemberInfoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.tproject.dao;
-
-import java.util.List;
-
-import com.tproject.dto.MemberInfoDTO;
-
-public interface MemberInfoDAO {
-	public List<MemberInfoDTO> getMemberInfo(String username);
-}
