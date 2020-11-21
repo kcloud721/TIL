6eea5ae09d8b7da6c969067f0c27a6bfdd81da9c
kcloud721 <kcloud721@naver.com>
2020-11-21 18:22:48 +0900

    Algo.

---
+++ algorithms/codeup/1027_16007115(AC).java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		String b[] = a.split("\\.");
+		System.out.printf("%s-%s-%s", b[2],b[1], b[0]);
+	}
+
+}
+
