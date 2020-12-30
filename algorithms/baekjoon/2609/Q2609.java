61b7fd869bbd5d20ab42293d35fcd6d5e60ef930
kcloud721 <kcloud721@naver.com>
2020-12-31 01:39:28 +0900

    Algo. 최대공약수/최소공배수

---
+++ algorithms/baekjoon/2609/Q2609.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int max = Math.max(n1, n2);
+        int min = Math.min(n1, n2);
+        int maxGys = recursive(max, min, -1);
+        int minGbs = n1 * n2 / maxGys;
+        System.out.println(maxGys);
+        System.out.println(minGbs);
+
+//        int max2 = Math.max(maxGys, n3);
+//        int min2 = Math.max(maxGys, n3);
+//        int maxGys2 = recursive(max2, min2, maxGys);
+//
+//        System.out.println(maxGys2);
+
+
+    }
+
+    public static int recursive(int max, int min, int sur){
+        if (sur == 0) {
+//            System.out.println(max + " " + min + " " + sur);
+            return max;
+        }
+        
+        sur = max%min;
+        max = min;
+        min = sur;
+
+        return recursive(max, min, sur);
+    }
+}
