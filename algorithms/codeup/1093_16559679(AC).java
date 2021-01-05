adb375ee838e07dbfcfd10587cfef83f95b96fa9
kcloud721 <kcloud721@naver.com>
2021-01-05 23:48:10 +0900

    Algo. codeup

---
+++ algorithms/codeup/1093_16559679(AC).java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[23+1];
+        for (int i = 1; i <= n; i++) {
+            arr[sc.nextInt()]++;
+        }
+
+        for (int i = 1; i <= 23 ; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
+
+    }
+}
+
---
+++ algorithms/codeup/1096_16560491(AC).java
@@ -0,0 +1,21 @@
+﻿import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[][] = new int[19][19];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[sc.nextInt()-1][sc.nextInt()-1] = 1;
+        }
+
+        for (int i = 0; i < 19; i++) {
+            for (int j = 0; j < 19; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
