6f9c6e0f90c1b36c846bf8a88791004c9be5505c
kcloud721 <kcloud721@naver.com>
2021-02-14 23:44:18 +0900

    무지의 먹방라이브 2

--- algorithms/programmers/42891/Q42891_2.java
+++ algorithms/programmers/42891/Q42891_2.java
@@ -7,6 +7,7 @@ public class Q42891_2 {
 	class Solution {
 	    public int solution(int[] food_times, long k) {
 	    	int answer = 0;
+	    	int accumMinus = 0;	//시간 차감값 누적 저장 변수
 	        ArrayList<foodInfo> arr = new ArrayList<>();
 	        
 	        // arr에 인덱스와 시간 넣어 초기화 
@@ -30,12 +31,29 @@ public class Q42891_2 {
 	        System.out.println(arr);
 	        
 	        for(int i=0; i<arr.size(); i++) {
-	        	int minusTime = arr.get(i).foodSize + (arr.size() - i);
+	        	int headTime = arr.get(i).foodSize - accumMinus; // 배열 처음에 있는 수 
+	        	int minusTime = 0; // 한번에 뺄 시간 
+	        	if(i == 0) {
+	        		minusTime = headTime * arr.size();
+	        	} else {
+	        		minusTime = (headTime - accumMinus) * (arr.size()-i);
+	        		System.out.println(minusTime);
+	        	}
+//	        	System.out.println("headTiem acc minustime " + headTime + " " + accumMinus + " " + minusTime);
+	        	
 	        	if(minusTime > k) {
-	        		answer = arr.get((int) (i+k)).index;
+	        		System.out.println(i + " " + k + " " + minusTime);
+	        		answer = arr.get((int) (i+(k%arr.size()-i))).index;
+	        		System.out.println("break " + answer);
 	        		break;
 	        	} else {
-	        		k -= minusTime;
+	        		k = k - minusTime;
+	        		accumMinus += headTime;
+	        		System.out.println("k minusTime " + k + " " + minusTime);
+	        	}
+	        	
+	        	if((i == arr.size()-1) && (k>0)) {
+	        		answer = -1;
 	        	}
 	        }
 	        
@@ -50,7 +68,7 @@ public class Q42891_2 {
 		Q42891_2.Solution sol = q.new Solution();
 		
 		int[] food_times = {3,1,2};
-		int k = 10;
+		int k = 5;
 		
 		System.out.println("ANSWER: " + sol.solution(food_times, k));
 
