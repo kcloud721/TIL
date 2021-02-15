a8c1d1fd0f2f0e0a86413c4c7e4dd95050dbedcb
kcloud721 <kcloud721@naver.com>
2021-02-15 23:49:30 +0900

    무지의 먹방라이브 2

--- algorithms/programmers/42891/Q42891_2.java
+++ algorithms/programmers/42891/Q42891_2.java
@@ -31,25 +31,29 @@ public class Q42891_2 {
 	        System.out.println(arr);
 	        
 	        for(int i=0; i<arr.size(); i++) {
-	        	int headTime = arr.get(i).foodSize - accumMinus; // 배열 처음에 있는 수 
+	        	int headTime = arr.get(i).foodSize; // 배열 처음에 있는 수 
 	        	int minusTime = 0; // 한번에 뺄 시간 
 	        	if(i == 0) {
-	        		minusTime = headTime * arr.size();
+	        		minusTime = (headTime - accumMinus) * arr.size();
 	        	} else {
 	        		minusTime = (headTime - accumMinus) * (arr.size()-i);
-	        		System.out.println(minusTime);
+//	        		System.out.println("arrsize " + arr.size() + " " + i);
 	        	}
+//	        	System.out.println("headT " + headTime + " accumMinus" + accumMinus);
+//        		System.out.println("minusTime " + minusTime);
 //	        	System.out.println("headTiem acc minustime " + headTime + " " + accumMinus + " " + minusTime);
 	        	
+        		System.out.println("headTime i k minusTIme  " + headTime + " " + i + " " + k + " " + minusTime);
+
 	        	if(minusTime > k) {
-	        		System.out.println(i + " " + k + " " + minusTime);
-	        		answer = arr.get((int) (i+(k%arr.size()-i))).index;
-	        		System.out.println("break " + answer);
+	        		int answerIndex = (int) (i + (k % (arr.size()-i)) - 1);
+	        		answer = arr.get(answerIndex).index;
+	        		System.out.println("break : answerIndex " + answerIndex);
 	        		break;
 	        	} else {
 	        		k = k - minusTime;
-	        		accumMinus += headTime;
-	        		System.out.println("k minusTime " + k + " " + minusTime);
+	        		accumMinus += (headTime - accumMinus);
+//	        		System.out.println("k : " + k);
 	        	}
 	        	
 	        	if((i == arr.size()-1) && (k>0)) {
@@ -67,8 +71,11 @@ public class Q42891_2 {
 		Q42891_2 q = new Q42891_2();
 		Q42891_2.Solution sol = q.new Solution();
 		
-		int[] food_times = {3,1,2};
-		int k = 5;
+//		int[] food_times = {4,2,3,6,7,1,5,8};
+//		int k = 27;
+		
+		int[] food_times = {1,1,1,1};
+		int k = 4;
 		
 		System.out.println("ANSWER: " + sol.solution(food_times, k));
 
